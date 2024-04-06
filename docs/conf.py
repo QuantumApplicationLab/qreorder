f3de38af555fb6db467c6b68533d18da9e49e37e
Esteban Aguilera <aguilera.ep@gmail.com>
2024-04-06 18:05:25 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,15 +18,15 @@
 # -- Project information -----------------------------------------------------
 
 project = "qreorder"
-copyright = "2024, Netherlands eScience Center"
-author = "Nicolas Renaud"
+copyright = "2024, Quantum Application Lab"
+author = "Quantum Application Lab"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = "0.1.0"
+version = "0.0.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
